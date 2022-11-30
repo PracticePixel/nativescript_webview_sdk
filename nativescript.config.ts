72ca2bd4bbd6b6979f4e94e0eff82bbfedc7169e
abdulaziz <aziz@marensolutions>
2022-11-30 17:11:45 +0530

    initial commit

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.example',
+  id: 'sa.litedev.sanar',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core'
+import { Button, EventData } from '@nativescript/core'
+
+import { Utils, Dialogs } from '@nativescript/core';
+import { InAppBrowser, NSAdvancedWebViewEventEmitter } from 'nativescript-inappbrowser';
 
 import { Item } from './item'
 import { ItemService } from './item.service'
@@ -9,10 +13,81 @@ import { ItemService } from './item.service'
 })
 export class ItemsComponent implements OnInit {
   items: Array<Item>
-
-  constructor(private itemService: ItemService) {}
+  
+  constructor(private itemService: ItemService) {
+    
+  }
 
   ngOnInit(): void {
     this.items = this.itemService.getItems()
   }
+
+  async onTap() {
+    const url = 'https://sanar.sa';
+    try {
+
+      NSAdvancedWebViewEventEmitter.once("ON_CLOSED", () => {
+        console.log('CLOSED');
+      });
+
+      NSAdvancedWebViewEventEmitter.once("LAUNCHED", () => {
+        console.log('LAUNCHED_FINISHED');
+      });
+
+      NSAdvancedWebViewEventEmitter.once("ERROR", () => {
+        console.log('LOAD_ERROR');
+      });
+
+      if (await InAppBrowser.isAvailable()) {
+        const result = await InAppBrowser.open(url, {
+          // iOS Properties
+          dismissButtonStyle: 'cancel',
+          preferredBarTintColor: '#453AA4',
+          preferredControlTintColor: 'white',
+          readerMode: false,
+          animated: true,
+          modalPresentationStyle: 'fullScreen',
+          modalTransitionStyle: 'coverVertical',
+          modalEnabled: true,
+          enableBarCollapsing: false,
+          // Android Properties
+          showTitle: false,
+          toolbarColor: '#6200EE',
+          secondaryToolbarColor: 'black',
+          navigationBarColor: 'black',
+          navigationBarDividerColor: 'white',
+          enableUrlBarHiding: true,
+          enableDefaultShare: true,
+          forceCloseOnRedirection: false,
+          // Specify full animation resource identifier(package:anim/name)
+          // or only resource name(in case of animation bundled with app).
+          animations: {
+            startEnter: 'slide_in_right',
+            startExit: 'slide_out_left',
+            endEnter: 'slide_in_left',
+            endExit: 'slide_out_right'
+          },
+          headers: {
+            'my-custom-header': 'my custom header value'
+          },
+          hasBackButton: true,
+          browserPackage: '',
+          showInRecents: false
+        });
+      }
+      else {
+        Utils.openUrl(url);
+      }
+    }
+    catch(error) {
+      Dialogs.alert({
+        title: 'Error',
+        message: error.message,
+        okButtonText: 'Ok'
+      });
+    }
+    
+  }
+
+
 }
